b4bb11a188a18d41c3a699e12c45cce0af9ac04b
root <jack@2cheekyltd.co.uk>
2019-10-28 05:40:32 -0700

    Cosmetic Changes

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,10 +4,9 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 @yield('head_meta')
 
-<link rel="shortcut icon" href="{{ asset('images/pokemon.png') }}">
+<link rel="shortcut icon" href="{{ asset('images/pokeball.png') }}">
 <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
-<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
 @yield('head_links')
 
 <script language="javascript" src="{{ asset('js/jquery.min.js') }}"></script>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,15 +3,16 @@
         <div class="row align-items-center no-gutters default-width-container">
             <div class="col-10">
                 <a href="{{ route('home') }}">
-                    <img src="{{ asset('images/pokeball.png') }}" height="75px" alt="Pokeball"/>
+                    <img src="{{ asset('images/pokemon.png') }}" height="75px" alt="Pokemon Logo"/>
                 </a>
             </div>
-            <div class="col-2">
-                <form class="form-inline my-2 my-lg-0" action="{{ route('search.post') }}" method="POST" class="search">
-                    <input class="form-control mr-sm-2" type="text" name="searchTerm" placeholder="Search by either name or ID">
-                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                    {{ csrf_field() }}
-                </form>
+            <div class="col-4 offset-md-4">
+                    <form class="form-inline my-2 my-lg-0" action="{{ route('search.post') }}" method="POST">
+                        <input class="form-control" type="text" name="searchTerm"
+                               placeholder="Search That Pokemon!">
+                        <button class="btn btn-secondary" type="submit"><i class="fas fa-search"></i></button>
+                        {{ csrf_field() }}
+                    </form>
             </div>
         </div>
     </div>
